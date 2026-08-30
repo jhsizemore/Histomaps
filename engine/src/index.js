891240e9870d98727c487c647ec156ef5c888130
jhsizemore <jhsizemore@gmail.com>
2026-08-30 17:42:19 +1100

    Export share context helpers from engine

--- engine/src/index.js
+++ engine/src/index.js
@@ -1,2 +1,6 @@
 export { sampleStream } from './stream-geometry.js';
 export { validateModel } from './validate-model.js';
+export { AGE_BANDS, classifyAge } from './age-bands.js';
+export { formatYear, formatYearRange, parseHistoricalYear } from './year-format.js';
+export { extractShareContext, defaultShareCopy } from './extract-share-context.js';
+export { SHARE_CARD_FORMATS, createShareCardPlan, drawShareCard } from './share-card.js';
